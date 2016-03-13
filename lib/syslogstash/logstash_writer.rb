a1d3b59ff0d27a1964e49ebbc19163318df3f809
Matt Palmer <mpalmer@hezmatt.org>
2016-03-14 10:15:15 +1100

    Ensure that exceptions in logstash writer get logged

--- lib/syslogstash/logstash_writer.rb
+++ lib/syslogstash/logstash_writer.rb
@@ -56,6 +56,9 @@ class Syslogstash::LogstashWriter
 					# If we got here, we sent successfully, so we don't want
 					# to put the entry back on the queue in the ensure block
 					entry = nil
+				rescue StandardError => ex
+					$stderr.puts "Unhandled exception: #{ex.message} (#{ex.class})"
+					$stderr.puts ex.backtrace.map { |l| "  #{l}" }.join("\n")
 				ensure
 					@entries_mutex.synchronize { @entries.unshift if entry }
 				end
